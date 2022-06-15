b39a5912c18d7fac4f315ebd679cf93f052ad402
Andrei Bucur <abucur980@gmail.com>
2022-06-15 09:16:03 +0300

    hide start button after click and also make input readonly

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ document.getElementById('button').addEventListener('click', addButtons);
 function addButtons() {
     // get the value from input
     let nButtons = document.getElementById('numOfButtons').value;
+    // hide start button after it was clicked and make input readonly to avoid further editing
+    this.style.display = 'none';
+    document.getElementById('numOfButtons').setAttribute('readonly', 'true');
     // generate n buttons
     for (let i = 0; i < nButtons; ++i) {
         document.getElementById('buttonContainer').insertAdjacentHTML("beforeend", '<button type="button" class="button btn btn-warning btn-lg m-2">Button</button>');
     }
     // get all buttons with the class button
-    let allButtons = document.querySelectorAll(".button");
+    var allButtons = document.querySelectorAll(".button");
     // generate the index for the lucky button
     let theLuckyOne = Math.floor(Math.random() * allButtons.length);
     // loop through the buttons and find the lucky one
